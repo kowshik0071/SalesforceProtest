8646bfb470569f29b7c719620b7b563aca1057e6
First Last <kowshikkows@gmail.com>
2021-06-09 22:48:35 +0530

    second test program

---
+++ Test/src/Selenium_tutorial/SimpleJavaprogram.java
@@ -0,0 +1,13 @@
+package Selenium_tutorial;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class SimpleJavaprogram {
+	WebDriver driver;
+	@Test
+	public void test1() {
+		System.out.println("Test");
+	}
+
+}
